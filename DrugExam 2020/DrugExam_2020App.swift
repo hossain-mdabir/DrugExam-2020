9785978953206bd9387b97d12cf9d94d25e73c9b
Md Abir Hossain <hossain.mdabir@gmail.com>
2023-03-29 22:31:35 +0600

    Initial Commit

---
+++ DrugExam 2020/DrugExam_2020App.swift
@@ -0,0 +1,17 @@
+//
+//  DrugExam_2020App.swift
+//  DrugExam 2020
+//
+//  Created by Md Abir Hossain on 29/3/23.
+//
+
+import SwiftUI
+
+@main
+struct DrugExam_2020App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
