594d29fbe8ea54c535ae6e0f52734e15d18d897c
Md Abir Hossain <hossain.mdabir@gmail.com>
2023-03-29 22:35:19 +0600

    Project created

---
+++ DrugExam 2020/Screens/LogInView.swift
@@ -0,0 +1,20 @@
+//
+//  LogInView.swift
+//  DrugExam 2020
+//
+//  Created by Md Abir Hossain on 29/3/23.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LogInView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogInView()
+    }
+}
